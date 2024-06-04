1e4380681e154c7d894d6e4ef7baeaa897e9f531
hasnain242 <78303526+hasnain242@users.noreply.github.com>
2024-06-05 04:57:29 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var a=10
+
+document.write('result'+'</br>'+'</br>'+`the value of a is ${a}`+'</br>')
+
+a=++a 
+document.write(`the value of ++a is:${a}`+'</br>')
+ a=a++;
+document.write(`the value of a++ is:${a}`+'</br>')
+a=--a;
+document.write(`the value of --a is:${a}`+'</br>')
+ a=a--;
+document.write(`the value of a--:${a}`)
+
+
+
+
+
